19f184a644aac1395592046aceb2ce839cf99fc8
alamareen <wial1001@hs-kl.de>
2020-06-08 16:42:39 +0000

    Update AlgorithmInfection.java

--- src/main/java/de/hskl/contol/AlgorithmInfection.java
+++ src/main/java/de/hskl/contol/AlgorithmInfection.java
@@ -1,6 +1,6 @@
 /*
  * MIT License
- * Copyright (c) 2020 Yannis Heim, Stefan Böbel
+ * Copyright (c) 2020 Wissam Aalamareen, Zouhir El Kharboubi
 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
